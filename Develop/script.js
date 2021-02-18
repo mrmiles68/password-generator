de6dffa7da96322f11740c0ce4d71d60127bef8b
mrmiles68 <cheaptechsupport2000@gmail.com>
2021-02-17 16:56:37 -0800

    used confirm instead of input code

--- Develop/script.js
+++ Develop/script.js
@@ -5,34 +5,33 @@ var lower = "abcdefghijklmnopqrstuvwxyz";
 var numer = "0123456789";
 var spec = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 var charType = 0;
-var assembly="";
-var passwordText="";
+var assembly = "";
+var passwordText = "";
+var alphaLength = 25
 
 
-console.log(spec.length);
 
 // Write password to the #password input
 function writePassword() {
+
+
+
   charType = Math.floor(Math.random() * 3);
-  if (lowchoice === "y" && charType === 0)
-   {
-  charIndex = Math.floor(Math.random() * 26);
-  passwordText.concat(assembly.charAt(charIndex));
+  if (lowChoice && charType === 0) {
+    charIndex = Math.floor(Math.random() * alphaLength);
+    passwordText.concat(lower.charAt(charIndex));
   }
-  if (upchoice === "y" && charType === 1)
-   {
-    charIndex = Math.floor(Math.random() * 26);
-    passwordText.concat(assembly,upper.charAt(charIndex));
+  if (upChoice && charType === 1) {
+    charIndex = Math.floor(Math.random() * alphaLength);
+    passwordText.concat(assembly, upper.charAt(charIndex));
   }
-  if (numchoice === "y" && charType === 2)
-   {
-      charIndex = Math.floor(Math.random() * 9);
-      passwordText.concat(assembly,numer.charAt(charIndex));
+  if (numChoice && charType === 2) {
+    charIndex = Math.floor(Math.random() * 9);
+    passwordText.concat(assembly, numer.charAt(charIndex));
   }
-  if (spechoice === "y" && charType === 3)
-   {
-        charIndex = Math.floor(Math.random() * 31);
-        passwordText.concat(assembly,spec.charAt(charIndex));
+  if (specialChoice && charType === 3) {
+    charIndex = Math.floor(Math.random() * 30);
+    passwordText.concat(assembly, spec.charAt(charIndex));
   }
 
   // var password = generatePassword();
@@ -46,32 +45,26 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Request user input
-var lowchoice = prompt("Require Lowercase characters? (Y/N)")
-lowchoice=lowchoice.toLowerCase()
-if (lowchoice != "y" && lowchoice !="n") {
-  lowchoice="y";
-} 
-var upchoice = prompt("Require Uppercase characters? (Y/N)")
-upchoice=upchoice.toLowerCase()
-if (upchoice != "y" && upchoice !="n") {
-  upchoice="y";
-} 
-var numchoice = prompt("Require Numerical characters? (Y/N)")
-numchoice=numchoice.toLowerCase()
-if (numchoice != "y" && numchoice !="n") {
-  numchoice="y";
-} 
-var spechoice = prompt("Require Special characters? (Y/N)")
-spechoice=spechoice.toLowerCase()
-if (spechoice != "y" && spechoice !="n") {
-  spechoice="y";
-} 
+var lowChoice = confirm("Require Lowercase characters?");
 
+var upChoice = confirm("Require Uppercase characters?");
 
-var length = Math.floor(Math.random() * 128) + 8;
-for (i=0; i < length; i++) {
-writePassword()
+var numChoice = confirm("Require Numerical characters?");
+
+var specialChoice = confirm("Require Special characters?")
+
+// Generate password length and run function
+var passLength = Math.floor(Math.random() * 128) + 8;
+for (i = 0; i < passLength; i++) {
+  writePassword()
 }
-console.log("length was " + length)
-console.log("password is: " + passwordText)
-console.log(assembly)
+// check variables
+console.log("place zero is " + lower[0])
+console.log("place zero is " + upper[0])
+console.log("place zero is " + numer[0])
+console.log("place one is " + spec[1]);
+console.log("length was " + passLength);
+console.log("password is: " + passwordText);
+console.log(passwordText);
+// var passwordText = document.querySelector("#password");
+// console.log(passwordTextElement);
