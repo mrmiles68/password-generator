dcef04c914fc65090c46b4a6e7f087a3659b16cb
mrmiles68 <cheaptechsupport2000@gmail.com>
2021-02-17 22:12:03 -0800

    Generate Button works now

--- Develop/script.js
+++ Develop/script.js
@@ -8,9 +8,15 @@ var charType = 0;
 var assembly = "";
 var passwordText = "";
 var alphaLength = 25
+var lowChoice = confirm("Require Lowercase characters?");
+var upChoice = confirm("Require Uppercase characters?");
+var numChoice = confirm("Require Numerical characters?");
+var specialChoice = confirm("Require Special characters?");
 
-
-
+var passLength = prompt("Length of Password? (8 - 128)")
+while (passLength < 8 || passLength > 128) {
+  passLength = prompt("Length of Password? (8 - 128)")
+}
 // Write password to the #password input
 function writePassword() {
 
@@ -19,19 +25,25 @@ function writePassword() {
   charType = Math.floor(Math.random() * 3);
   if (lowChoice && charType === 0) {
     charIndex = Math.floor(Math.random() * alphaLength);
-    passwordText.concat(lower.charAt(charIndex));
+    passwordText += lower.charAt(charIndex);
+
   }
   if (upChoice && charType === 1) {
     charIndex = Math.floor(Math.random() * alphaLength);
-    passwordText.concat(assembly, upper.charAt(charIndex));
+    passwordText += upper.charAt(charIndex);
+
   }
+
   if (numChoice && charType === 2) {
     charIndex = Math.floor(Math.random() * 9);
-    passwordText.concat(assembly, numer.charAt(charIndex));
+    passwordText += numer.charAt(charIndex);
+
   }
+
   if (specialChoice && charType === 3) {
     charIndex = Math.floor(Math.random() * 30);
-    passwordText.concat(assembly, spec.charAt(charIndex));
+    passwordText += spec.charAt(charIndex);
+
   }
 
   // var password = generatePassword();
@@ -42,29 +54,39 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function () {
+
+  for (i = 0; i < passLength; i++) {
+
+    writePassword()
+
+  }
+  document.getElementById('password').value = passwordText;
+  console.log("place zero is " + lower[0])
+  console.log("place zero is " + upper[0])
+  console.log("place zero is " + numer[0])
+  console.log("place one is " + spec[1]);
+  console.log("length was " + passLength);
+  console.log("password is: " + passwordText);
+  console.log(passwordText);
+
+});
 
 // Request user input
-var lowChoice = confirm("Require Lowercase characters?");
 
-var upChoice = confirm("Require Uppercase characters?");
 
-var numChoice = confirm("Require Numerical characters?");
 
-var specialChoice = confirm("Require Special characters?")
+
 
 // Generate password length and run function
-var passLength = Math.floor(Math.random() * 128) + 8;
-for (i = 0; i < passLength; i++) {
-  writePassword()
-}
+// var passLength = Math.floor(Math.random() * 128) + 8;
+// for (i = 0; i < passLength; i++) {
+//   writePassword()
+// }
+// document.getElementById('password').value = passwordText;
+
 // check variables
-console.log("place zero is " + lower[0])
-console.log("place zero is " + upper[0])
-console.log("place zero is " + numer[0])
-console.log("place one is " + spec[1]);
-console.log("length was " + passLength);
-console.log("password is: " + passwordText);
-console.log(passwordText);
+
+// var passwordText = document.querySelector("#password");
 // var passwordText = document.querySelector("#password");
 // console.log(passwordTextElement);
